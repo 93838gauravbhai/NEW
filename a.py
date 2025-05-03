c2a930ce83c68fc166624e0fa708df4d957f2cfa
93838gauravbhai <Ga.urav.3.6546@gmail.com>
2025-05-03 14:40:11 +0530

    Update a.py

--- a.py
+++ a.py
@@ -42,7 +42,7 @@ redeem_codes_collection = db['redeem_codes0']
 
 # Bot Configuration
 
-TELEGRAM_BOT_TOKEN = '7503879369:AAE9Iey42lR4WZRCO-xLHY3QbzMc1Rl6Hek'
+TELEGRAM_BOT_TOKEN = '7567349538:AAEgRwaWf-YtCtHRB_BSdj0IrfrArAF7fks'
 
 ADMIN_USER_ID = 6539807903 # Replace with your admin user ID
 
